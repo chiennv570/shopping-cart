d817ce04a22e1330f0031b167800f920d97b72f8
Chien PHP <chiennv570@gmail.com>
2017-03-19 18:27:44 +0700

    Product Controller and Views

---
+++ backend/views/product-category/index.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\ProductCategorySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Product Categories';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-category-index">
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?php echo Html::a('Create Product Category', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php echo GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'status',
+            'created_at',
+            'updated_at',
+            // 'created_by',
+            // 'updated_by',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+</div>
